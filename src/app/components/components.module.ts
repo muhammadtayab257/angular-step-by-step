2a5ce195a921be0f94d12ce625f618c2dcfde50a
Tayyab <muhammadtayab3022@gmail.com>
2021-09-26 13:27:14 +0500

    new

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -7,10 +7,6 @@ import { ComponentRoutingModule } from './Components.routing.module'
 import { HomeComponent } from './home/home.component';
 import { ParentComponent } from './Easy-Topics/Components-Interactions/parent/parent.component';
 import { ChildComponent } from './Easy-Topics/Components-Interactions/parent/child/child.component';
-import { DataBindingsComponent } from './Easy-Topics/data-bindings/data-bindings.component';
-import { PropertyBindingsComponent } from './Easy-Topics/property-bindings/property-bindings.component';
-import { AngularDirectivesComponent } from './Easy-Topics/angular-directives/angular-directives.component';
-import { LifeCycleHooksComponent } from './Easy-Topics/life-cycle-hooks/life-cycle-hooks.component';
 import { PromisesComponent } from './Hard-Topics/Rxjs/promises/promises.component';
 import { AsyncAwaitComponent } from './Hard-Topics/Rxjs/async-await/async-await.component';
 import { FromEventComponent } from './Hard-Topics/Rxjs/from-event/from-event.component';
@@ -23,10 +19,6 @@ import { ListComponent } from './Hard-Topics/Rxjs/list/list.component';
     HomeComponent,
     ParentComponent,
     ChildComponent,
-    DataBindingsComponent,
-    PropertyBindingsComponent,
-    AngularDirectivesComponent,
-    LifeCycleHooksComponent,
     PromisesComponent,
     AsyncAwaitComponent,
     FromEventComponent,
@@ -42,7 +34,6 @@ import { ListComponent } from './Hard-Topics/Rxjs/list/list.component';
     HomeComponent,
     ParentComponent,
     ChildComponent,
-    DataBindingsComponent,
     PromisesComponent,
     AsyncAwaitComponent,
     FromEventComponent,
